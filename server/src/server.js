8fcfbceb11c5161263e8305343e64477506a099e
akankshamv320-commits <akankshamv320@gmail.com>
2026-09-24 20:06:02 +0530

    Create server.js

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  res.end(
+    JSON.stringify({
+      message: "Accessibility Audit API is running"
+    })
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
